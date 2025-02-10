c350c7795a6f0347236ca8a8a115e05cf9b2f216
lesmana-arch <hapizlesmana@Gmail.com>
2025-02-10 18:28:06 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const SETTINGS = {
   openDays: [1, 2, 3, 4, 6, 7, 0],
   openTime: "08:00",
-  closeTime: "19:00"
+  closeTime: "18:00"
 };
 //INI KODE CONFIG YANG BERFUNGSI!!!!!
 //SETELAH DI EDIT BUTUH WAKTU SEKITAR 5 MENIT UNTUK MELIHAT PERUBAHAN DI WEBSITE
